48397900028ffc036b8c62c0fc62e3dd7010cd5f
sayyadirfanali <irfan@irfanali.org>
2025-11-10 00:14:32 +0530

    potential fix for #9

---
+++ vert_compact.py
@@ -0,0 +1,27 @@
+import fontforge
+
+font = fontforge.open("./Myna.sfd")
+
+ascent  = font.ascent
+descent = font.descent
+
+RATIO = 0.9
+
+# general
+font.ascent  = int(font.ascent * RATIO)
+font.descent = int(font.descent * RATIO)
+
+# hhea
+font.hhea_ascent  = int(font.hhea_ascent * RATIO)
+font.hhea_descent = int(font.hhea_descent * RATIO)
+
+# OS/2 typo
+font.os2_typoascent  = font.hhea_ascent
+font.os2_typodescent = font.hhea_descent
+
+# OS/2 win
+font.os2_winascent = ascent # can increase or decrease this if clipped 
+font.os2_windescent = descent
+
+font.save("Myna.sfd")
+font.close()
